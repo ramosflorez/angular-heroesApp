5cd0fe65e54a1985d75703278064e245161c6436
Valeria Ramos <valeria.r@eyeride.io>
2024-11-07 17:59:57 -0500

    avance

--- src/app/heroes/heroes-routing.module.ts
+++ src/app/heroes/heroes-routing.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { HeroPageComponent } from './hero-page/hero-page.component';
 
 const routes: Routes = [
   {
@@ -11,7 +14,28 @@ const routes: Routes = [
       {
         path:'new-hero',
         component: NewPageComponent
+      },
+      {
+        path:'search',
+        component:SearchPageComponent
+      },
+      {
+        path:'edit/:id',
+        component:NewPageComponent
+      },
+      {
+        path:'list',
+        component:ListPageComponent
+      },
+      {
+        path:':id',
+        component:HeroPageComponent
+      },
+      {
+        path:'**',
+        redirectTo:'list'
       }
+
     ]
   }
 ];
--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -3,11 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { HeroesRoutingModule } from './heroes-routing.module';
 import { HeroPageComponent } from './hero-page/hero-page.component';
+import { RouterModule } from '@angular/router';
+import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
+import { ListPageComponent } from './pages/list-page/list-page.component';
+import { NewPageComponent } from './pages/new-page/new-page.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
 
 @NgModule({
   declarations: [
-    HeroPageComponent
+    HeroPageComponent,
+    HeroPageComponent,
+    LayoutPageComponent,
+    ListPageComponent,
+    NewPageComponent,
+    SearchPageComponent
+
   ],
   imports: [
     CommonModule,
